0645a26f775639164218e51670df56864addde67
Ky <kmallonn2021@fau.edu>
2023-10-05 16:21:25 -0400

    Completed step 4
    Only "show" posts shared within 24 hours of the current user's last post, otherwise blur out the post image.

--- lab_6/lab-insta-parse/Feed/PostCell.swift
+++ lab_6/lab-insta-parse/Feed/PostCell.swift
@@ -55,7 +55,28 @@ class PostCell: UITableViewCell {
         }
         
         // TODO: Pt 2 - Show/hide blur view
-        
+        // A lot of the following returns optional values so we'll unwrap them all together in one big `if let`
+        // Get the current user.
+        if let currentUser = User.current,
+
+            // Get the date the user last shared a post (cast to Date).
+           let lastPostedDate = currentUser.lastPostedDate,
+
+            // Get the date the given post was created.
+           let postCreatedDate = post.createdAt,
+
+            // Get the difference in hours between when the given post was created and the current user last posted.
+           let diffHours = Calendar.current.dateComponents([.hour], from: postCreatedDate, to: lastPostedDate).hour {
+
+            // Hide the blur view if the given post was created within 24 hours of the current user's last post. (before or after)
+            blurView.isHidden = abs(diffHours) < 24
+        } else {
+
+            // Default to blur if we can't get or compute the date's above for some reason.
+            blurView.isHidden = false
+        }
+
+
         
     }
     
